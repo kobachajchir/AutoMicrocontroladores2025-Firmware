402a30a2acc7a63a6c37a774f991a697e6138c91
Koba Chajchir <65787206+kobachajchir@users.noreply.github.com>
2026-01-13 15:58:57 -0300

    no usb anymore until errors fix

---
+++ Debug/Drivers/STM32F1xx_HAL_Driver/stm32f1xx_hal_dma.d
@@ -0,0 +1,4 @@
+Drivers/STM32F1xx_HAL_Driver/stm32f1xx_hal_dma.o: \
+ C:/Users/kobac/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c \
+ C:/Users/kobac/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+C:/Users/kobac/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
