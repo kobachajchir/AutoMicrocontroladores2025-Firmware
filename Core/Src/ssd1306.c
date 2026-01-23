a3ae070df23f3ec04c31cf053381d170dee34ae8
Koba Chajchir <65787206+kobachajchir@users.noreply.github.com>
2026-01-22 22:05:36 -0300

    Guard OLED TX inflight sequencing

--- Core/Src/ssd1306.c
+++ Core/Src/ssd1306.c
@@ -76,6 +76,7 @@ static uint8_t oled_inflight_slot = 0xFFu;
 static uint8_t oled_inflight_page = 0;
 static uint8_t oled_inflight_keep_bus = 0;
 static uint8_t oled_inflight_active = 0;
+static uint8_t oled_tx_inflight = 0;
 static uint8_t oled_pages_in_lease = 0;
 static const uint8_t oled_max_pages_per_lease = 4;
 
@@ -111,6 +112,7 @@ static void OLED_StartFrame(uint8_t page)
   oled_page = page;
   oled_sm = OLED_SM_CMD_PAGE;
   oled_inflight_active = 1;
+  oled_tx_inflight = 1;
 
   __NOP(); // BREAKPOINT: inicio de frame OLED (page 0)
   oled_cmd_byte = (uint8_t)(0xB0u + oled_page);
@@ -251,9 +253,10 @@ static void OLED_I2C_TxComplete(void *ctx)
   if (g_i2c_mgr && I2C_Manager_GetOwner(g_i2c_mgr) != g_oled_dev_id) {
     return;
   }
-  if (oled_sm == OLED_SM_IDLE || !oled_inflight_active) {
+  if (oled_sm == OLED_SM_IDLE || !oled_inflight_active || !oled_tx_inflight) {
     return;
   }
+  oled_tx_inflight = 0;
   OLED_I2C_TransferComplete();
 }
 
@@ -274,6 +277,7 @@ static void OLED_I2C_TransferComplete(void)
       oled_sm = OLED_SM_CMD_LOWCOL;
       __NOP(); // BREAKPOINT: comando de low column
       oled_cmd_byte = (uint8_t)SETLOWCOLUMN;
+      oled_tx_inflight = 1;
       (void)HAL_I2C_Mem_Write_DMA(&SSD1306_I2C_PORT, SSD1306_I2C_ADDR, 0x00, 1, &oled_cmd_byte, 1);
       break;
 
@@ -281,12 +285,14 @@ static void OLED_I2C_TransferComplete(void)
       oled_sm = OLED_SM_CMD_HIGHCOL;
       __NOP(); // BREAKPOINT: comando de high column
       oled_cmd_byte = (uint8_t)SETHIGHCOLUMN;
+      oled_tx_inflight = 1;
       (void)HAL_I2C_Mem_Write_DMA(&SSD1306_I2C_PORT, SSD1306_I2C_ADDR, 0x00, 1, &oled_cmd_byte, 1);
       break;
 
     case OLED_SM_CMD_HIGHCOL:
       oled_sm = OLED_SM_DATA;
       __NOP(); // BREAKPOINT: envío de datos de página
+      oled_tx_inflight = 1;
       (void)HAL_I2C_Mem_Write_DMA(&SSD1306_I2C_PORT, SSD1306_I2C_ADDR, 0x40, 1,
                                   &SSD1306_Buffer[SSD1306_WIDTH * oled_page],
                                   width());
@@ -351,6 +357,7 @@ static void OLED_I2C_Error(void *ctx, I2C_ManagerError err)
   (void)ctx;
   (void)err;
   oled_sm = OLED_SM_IDLE;
+  oled_tx_inflight = 0;
   if (g_i2c_mgr) {
     (void)I2C_Manager_ReleaseBus(g_i2c_mgr, g_oled_dev_id);
   }
